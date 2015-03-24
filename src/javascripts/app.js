c6507c0ce1e76cf00dcacd14a0c2f19d750d89eb
Joshua Bradley <josh.u.war@gmail.com>
2015-03-24 18:00:04 +0000

    adds round buttons, menu link animation, sticky header

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,4 +1,14 @@
 
 $(document).ready(function() {
 	$('.gramble-item .excerpt').matchHeight();
+	$('.sticky-header').stickThis();
+
+	// using: https://github.com/callmenick/Animating-Hamburger-Icons/blob/master/js/demo/demo.js
+	$(".cmn-toggle-switch").click(function(e) {
+		e.preventDefault();
+		(this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+	});
 });
+
+
+
