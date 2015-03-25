d4409a573b8b45d47b482f269cd99a396a1849ca
Joshua Bradley <josh.u.war@gmail.com>
2015-03-25 11:33:54 +0000

    adds menu styles

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -8,6 +8,25 @@ $(document).ready(function() {
 		e.preventDefault();
 		(this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
 	});
+
+	// derived from http://codyhouse.co/gem/full-screen-pushing-navigation/
+
+	var isLateralNavAnimating = false;
+	
+	//open/close lateral navigation
+	$('.cmn-toggle-switch').on('click', function(event){
+		event.preventDefault();
+		//stop if nav animation is running 
+		if( !isLateralNavAnimating ) {
+			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
+			
+			$('body').toggleClass('nav-menu-open');
+			$('.nav-menu-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				//animation is over
+				isLateralNavAnimating = false;
+			});
+		}
+	});
 });
 
 
